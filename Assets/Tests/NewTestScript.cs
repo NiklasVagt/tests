418379ab675e70189511acad2ebda8d4d23b03bb
Vagt <vagt_n@mslocal.de>
2023-10-17 12:43:45 +0200

    init

---
+++ Assets/Tests/NewTestScript.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class NewTestScript
+{
+
+    [SetUp]
+    public void ResetScene()
+    {
+        EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
+    }
+
+    [Test]
+    public void SimpleTest()
+    {
+        bool isActive = false;
+
+        Assert.AreEqual(false, isActive);
+    }
+
+    [Test]
+    public void CatchingErrors()
+    {
+
+        GameObject gameObject = new GameObject("test");
+
+        Assert.Throws<MissingComponentException>(
+            () => gameObject.GetComponent<Rigidbody>().velocity = Vector3.one
+        );
+
+    }
+}
